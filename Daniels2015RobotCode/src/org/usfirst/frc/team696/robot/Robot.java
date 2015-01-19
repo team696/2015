48e84c9a218626469fece74f57085be0a54f1666
dtorres99 <disidrot@gmail.com>
2015-01-18 19:09:24 -0800

    Daniel added "Daniels2015RobotCode" which has a working swerve tracker(code will be cleaned up for next commit)

---
+++ Daniels2015RobotCode/src/org/usfirst/frc/team696/robot/Robot.java
@@ -0,0 +1,118 @@
+
+package org.usfirst.frc.team696.robot;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+	Elevator elevator = new Elevator();
+	Joystick control = new Joystick(1);
+	Encoder elevEnc = new Encoder(1,2);
+	AnalogInput encOne = new AnalogInput(0);
+	AnalogInput encTwo = new AnalogInput(1);
+	AnalogInput encThree = new AnalogInput(2);
+	AnalogInput encFour = new AnalogInput(3);
+	double goal;
+	DigitalInput atBot = new DigitalInput(0);
+	VictorSP elevMotor = new VictorSP(0);
+	int[] count = new int[4];
+	SwerveTracking one = new SwerveTracking();
+	SwerveTracking two = new SwerveTracking();
+	SwerveTracking three = new SwerveTracking();
+	SwerveTracking four = new SwerveTracking();
+	double[] wheelEnc = new double[4];
+	double[] oldWheelEnc = new double[4];
+	int[] forceCounter = new int[4];
+	
+    public void robotInit() {
+    	one.forceCount(forceCounter[0]);
+    	two.forceCount(forceCounter[1]);
+    	three.forceCount(forceCounter[2]);
+    	four.forceCount(forceCounter[3]);
+    }
+
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+    	//elevator code
+    	
+//        goal+= (control.getRawAxis(4)/4);
+//        elevator.set(elevEnc.get(), goal, atBot.get());
+//        elevMotor.set(elevator.move());
+//        if (elevator.shouldReset()) elevEnc.reset();
+         
+        //SwerveTracking code        
+    	for(int fish = 0; fish < 4;fish++){
+			oldWheelEnc[fish] = wheelEnc[fish];
+		}
+    	
+    	wheelEnc[0] = encOne.getVoltage();
+    	wheelEnc[1] = encTwo.getVoltage();
+    	wheelEnc[2] = encThree.getVoltage();
+    	wheelEnc[3] = encFour.getVoltage();
+    	
+    	one.set(oldWheelEnc[0], wheelEnc[0]);
+    	two.set(oldWheelEnc[1], wheelEnc[1]);
+    	three.set(oldWheelEnc[2], wheelEnc[2]);
+    	four.set(oldWheelEnc[3], wheelEnc[3]);
+        
+    	count[0]=one.countNum();
+    	count[1]=two.countNum();
+    	count[2]=three.countNum();
+    	count[3]=four.countNum();
+    	
+    	//writing to and getting from smart dash  board    	
+    	String writeToFile = count[0] + " " + count[1] + " " + count[2]+ " " + count[3] + "";
+    	SmartDashboard.putString("appendToFile",writeToFile);
+    	String readFromFile = SmartDashboard.getString("readFromFile", "7 7 7 7");
+    	
+    	
+    	//force counter set
+    	
+    	forceCounter[0] = Integer.parseInt(readFromFile.substring(0,1));
+    	forceCounter[1] = Integer.parseInt(readFromFile.substring(2,3));
+    	forceCounter[2] = Integer.parseInt(readFromFile.substring(4,5));
+    	forceCounter[3] = Integer.parseInt(readFromFile.substring(6));
+    	
+    	System.out.print(encOne.getVoltage() + "  ");
+    	for(int fish = 0; fish < 4;fish++){
+    		System.out.print(forceCounter[fish] + "  ");
+		}
+    	System.out.println();
+    	
+    	
+    }
+    
+    /**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+    
+    }
+    
+}
+ 
---
+++ Daniels2015RobotCode/src/org/usfirst/frc/team696/robot/SwerveTracking.java
@@ -0,0 +1,40 @@
+package org.usfirst.frc.team696.robot;
+
+public class SwerveTracking {
+	private double curPos;
+	private double disPerSec = 102.85714285714285714285714285714;
+	private int counter = 0;
+	private double oldIn;
+	private double newIn;
+	
+	public void set(double _oldIn,double _newIn) {
+		this.curPos = Util.map(_newIn, 0, 5, 0, 360);
+		this.oldIn = _oldIn;
+		this.newIn = _newIn;
+	}
+	
+	private void tracker() {
+		boolean testClockWise = oldIn<0.1 && newIn>4.9;
+		boolean testCounterClockWise = oldIn>4.9 && newIn<.1;
+		if(testClockWise) this.counter-=1;
+		if(testCounterClockWise) this.counter+=1;
+		if(counter == 7) this.counter = 0;
+		if(counter==-1)this.counter=6;
+	}
+
+	public double pos() {
+		return disPerSec*this.counter + (this.curPos/7)*2;	
+	}
+	
+	public void forceCount(int _counter) {
+		this.counter = _counter;
+	}
+	
+	public int countNum() {
+		tracker();
+		return this.counter;
+	}
+	
+	
+	
+}

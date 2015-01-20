b51705862b3183692de4cfcb9156f56f821884ce
dtorres99 <disidrot@gmail.com>
2015-01-19 16:19:19 -0800

    Added JavaReminder.java

---
+++ Daniels2015RobotCode/src/org/usfirst/frc/team696/robot/JavaReminder.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team696.robot;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class JavaReminder {
+	Timer timer;
+	
+	public JavaReminder(int seconds) {
+		timer = new Timer();
+		timer.schedule(new RemindTask(), seconds * 1000);
+	}
+	
+	class RemindTask extends TimerTask {
+		
+		@Override
+		public void run() {
+			SwerveTracking.tracker();
+			timer.cancel();
+		}
+	}
+}

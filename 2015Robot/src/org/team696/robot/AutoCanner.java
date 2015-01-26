28d90ae944efce790afbc1da820d28454f793410
dtorres99 <disidrot@gmail.com>
2015-01-26 08:45:02 -0800

    Finished AutoCanner. Ready to test

--- 2015Robot/src/org/team696/robot/AutoCanner.java
+++ 2015Robot/src/org/team696/robot/AutoCanner.java
@@ -1,5 +1,41 @@
 package org.team696.robot;
 
-public class AutoCanner {
+import org.team696.baseClasses.Runnable;
+
+import edu.wpi.first.wpilibj.Solenoid;
+
+public class AutoCanner extends Runnable {
+	Solenoid leftGrab;
+	Solenoid rightGrab;
 	
+	boolean leftOut = false;
+	boolean rightOut = false;
+	
+	/*
+	 * @param config - leftGrab, rightGrab
+	 */
+	public AutoCanner(int[] config) {
+		leftGrab = new Solenoid(config[0]);
+		rightGrab = new Solenoid(config[1]);
+	}
+	
+	@Override
+	public void start(int periodMS) {
+		super.start(periodMS);
+	}
+	
+	@Override
+	public void update() {
+		run();
+	}
+	
+	public void set(boolean[] extend) {
+		leftOut = extend[0];
+		rightOut = extend[1];
+	}
+	
+	public void run() {
+		leftGrab.set(leftOut);
+		rightGrab.set(rightOut);
+	}
 }

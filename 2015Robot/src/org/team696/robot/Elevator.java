6275e35119a7e0146518ce937a82a1ed296b19b9
dtorres99 <disidrot@gmail.com>
2015-01-25 17:26:04 -0800

    Added break system to 2015Robot ELevator

--- 2015Robot/src/org/team696/robot/Elevator.java
+++ 2015Robot/src/org/team696/robot/Elevator.java
@@ -5,6 +5,7 @@ import org.team696.baseClasses.Runnable;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.VictorSP;
 
 public class Elevator extends Runnable {
@@ -13,12 +14,14 @@ public class Elevator extends Runnable {
 	DigitalInput limitSwitchTop;
 	VictorSP elevMotor;
 	CustomPID heightController;
+	Solenoid breaker;
 	
+	boolean startBreaking;
 	double cliksPerTote = 360;
 	double goalPos;	
 
 	/*
-	 * @param config - encoderSlotA, encoderSlotB, limitSwitchBot, limitSwitchTop, Kp, Ki, Kd
+	 * @param config - encoderSlotA, encoderSlotB, limitSwitchBot, limitSwitchTop, Kp, Ki, Kd, BreakerChannel
 	 */
 	public Elevator(int[] config) {
 		encoder = new Encoder(config[0], config[1]);
@@ -27,6 +30,8 @@ public class Elevator extends Runnable {
 		limitSwitchTop = new DigitalInput(config[3]);
 		
 		heightController = new CustomPID(config[4], config[5], config[6]);
+		
+		breaker = new Solenoid(config[7]);
 	}
 
 	@Override
@@ -38,11 +43,22 @@ public class Elevator extends Runnable {
 		goalPos = _goalPos;
 	}
 	
+	public void setBreaking(boolean _startBreaking) {
+		startBreaking = _startBreaking;
+	}
+	
 	public void setHeight() {
+		breakSys();
 		if (limitSwitchTop.get() && goalPos>cliksPerTote*6.5) elevMotor.set(0);
 		else if (limitSwitchBot.get()) encoder.reset();
 		else if (limitSwitchBot.get() && goalPos<0) elevMotor.set(0);
 		else elevMotor.set(goalPos - encoder.get());
+	}
+	
+	public void breakSys() {
+		if (startBreaking) breaker.set(true);
+		
+		breaker.set(false);
 	}
 	
 	@Override

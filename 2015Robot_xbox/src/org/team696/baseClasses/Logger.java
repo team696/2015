a6a246d219c8113a20522b7f1ebeefb23d9feb1c
Daniel <disidrot@gmail.com>
2015-12-04 14:02:20 -0800

    Added 2015Robot_xbox which has full control minus the elevator

---
+++ 2015Robot_xbox/src/org/team696/baseClasses/Logger.java
@@ -0,0 +1,264 @@
+package org.team696.baseClasses;
+
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import edu.wpi.first.wpilibj.Timer;
+
+public class Logger extends Runnable {
+	public FileWriter writer;
+	BufferedWriter bw;
+	FileReader reader;
+	Timer timer = new Timer();
+	BufferedReader br;
+	String[] names; 
+	String[] values;
+	String toSend;
+	public String fn;
+	boolean dontPut = false;
+	
+	public String getDate(){
+		Date date = new Date();
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+		return df.format(date);
+	}
+	
+	public String[] read(int lines) throws IOException{
+		String[] iRead = new String[lines];
+		for(int fish=0;fish<lines;fish++){
+			iRead[fish]=br.readLine();
+		}
+		return iRead;
+	}
+	
+	public Logger(String[] configName, String _fn) {
+		fn = _fn;
+		
+		makeReader();
+		
+		names = new String[configName.length];
+		values = new String[names.length];
+		for(int fish = 0; fish < configName.length;fish++){
+			names[fish] = configName[fish];
+		}
+	}
+	
+	public void makeWriter(){
+		try{
+			writer = new FileWriter(fn,false);
+			bw = new BufferedWriter(writer);
+		}catch(IOException e){e.printStackTrace();}
+	}
+	
+	public void makeReader(){
+		try{
+			reader = new FileReader(fn);
+			br = new BufferedReader(reader);
+		} catch(FileNotFoundException e){
+			e.printStackTrace();
+			makeWriter();
+			try{
+				String s = "0";
+				writer.write(s);
+				writer.flush();
+				
+				reader = new FileReader(fn);
+				br = new BufferedReader(reader);
+			}catch(IOException f){f.printStackTrace();}
+		}
+	}
+	
+	public void writerRefresh(){
+		try{
+			writer = new FileWriter(fn,false);
+		}catch(IOException e){System.out.println("Error");};
+	}
+
+	public void init() {
+	}
+	
+	@Override
+	public void start(int periodMS){
+		super.start(periodMS);
+		toSend = "";
+		timer.start();
+	}
+	
+	@Override
+	public void update(){
+		setString();
+	}
+	
+	public void stop(){
+		timer.stop();
+		timer.reset();
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void set(int val,int pos){
+		values[pos] = val+"";
+		if (values[pos].length() >= 5)values[pos]=values[pos].substring(0, 5);
+		if(values[pos].length()<5){
+			int lenDif =5-values[pos].length();
+			for(int fish = 0; fish < lenDif;fish++){
+				values[pos]+=" ";
+			}
+		}
+	}
+	
+	public void set(String val,int pos){
+		values[pos] = val+"";
+		if (values[pos].length() >= 10)values[pos]=values[pos].substring(0, 10);
+		if(values[pos].length()<10){
+			int lenDif =10-values[pos].length();
+			for(int fish = 0; fish < lenDif;fish++){
+				values[pos]+=" ";
+			}
+		}
+	}
+	
+	public void set(double val,int pos){
+		values[pos] = val+"";
+		if (values[pos].length() >= 5)values[pos]=values[pos].substring(0, 5);
+		if(values[pos].length()<5){
+			int lenDif = 5-values[pos].length();
+			for(int fish = 0; fish < lenDif;fish++){
+				values[pos]+=" ";
+			}
+		}
+	}
+	
+	public void set(boolean val,int pos){
+		values[pos] = val+"";
+		if (values[pos].length() > 5)values[pos]=values[pos].substring(0, 5);
+		if(values[pos].length()<5){
+			for(int fish = 0; fish < 5-values[pos].length();fish++){
+				values[pos]+=" ";
+			}
+		}
+	}
+	
+	public void set(float val,int pos){
+		values[pos] = val+"";
+		if (values[pos].length() > 5)values[pos]=values[pos].substring(0, 5);
+		if(values[pos].length()<5){
+			for(int fish = 0; fish < 5-values[pos].length();fish++){
+				values[pos]+=" ";
+			}
+		}
+	}
+	
+	public String setTime() {
+		dontPut = false;
+		if (timer.get() < 0.1)dontPut = true;
+		String time = timer.get()+" | ";
+		if (time.length() > 5)time=time.substring(0, 5);
+		if(time.length()<5){
+			for(int fish = 0; fish < 5-time.length();fish++){
+				time+=" ";
+			}
+		}
+		return time;
+	}
+	
+	public void setString(boolean noTime){
+		toSend = "";
+		if (!noTime)setString();
+		else {
+			for(int fish = 0; fish < names.length;fish++){
+				toSend = toSend + names[fish] + ": " + values[fish] + " | ";
+			}
+		}
+		sendString();
+	}
+	
+	private void setString(){
+		toSend = "time: "+ setTime() + " | ";
+		for(int fish = 0; fish < names.length;fish++){
+			toSend = toSend + names[fish] + ": " + values[fish] + " | ";
+		}
+		toSend+="\n";
+		sendString();
+	}
+	
+	public void write(boolean f){
+		if(!dontPut){
+			try{
+				bw.write(f+"");
+				bw.flush();
+			}catch(IOException e){e.printStackTrace();}
+		} else {
+//			System.out.println("Not Writing");
+		}
+	}
+	
+	public void write(float f){
+		if(!dontPut){
+			try{
+				bw.write(f+"");
+				bw.flush();
+			}catch(IOException e){e.printStackTrace();}
+		} else {
+//			System.out.println("Not Writing");
+		}
+	}
+	
+	public void write(double d){
+		if(!dontPut){
+			try{
+				bw.write(d+"");
+				bw.flush();
+			}catch(IOException e){e.printStackTrace();}
+		} else {
+//			System.out.println("Not Writing");
+		}
+	}
+	
+	public void write(int i){
+		if(!dontPut){
+			try{
+				bw.write(i+"");
+				bw.flush();
+			}catch(IOException e){e.printStackTrace();}
+		} else {
+//			System.out.println("Not Writing");
+		}
+	}
+	
+	public void write(String str){
+		if(!dontPut){
+			try{
+				bw.write(str);
+				bw.flush();
+				
+			}catch(IOException e){e.printStackTrace();}
+		} else {
+//			System.out.println("Not Writing");
+		}
+	}
+	
+	public void sendString(){
+			write(toSend);
+	}
+	
+	public void closeWriter(){
+		try{
+			bw.close();
+			writer.close();
+		}catch(IOException e){e.printStackTrace();}
+		
+	}
+	
+	public void closeReader(){
+		try {
+			br.close();
+			reader.close();
+		}catch(IOException e){e.printStackTrace();}
+	}
+}
---
+++ 2015Robot_xbox/src/org/team696/subsystems/SteeringEncoder.java
@@ -0,0 +1,133 @@
+package org.team696.subsystems;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import org.team696.baseClasses.Logger;
+import org.team696.baseClasses.Runnable;
+import org.team696.baseClasses.Util;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.AnalogTrigger;
+import edu.wpi.first.wpilibj.AnalogTriggerOutput;
+import edu.wpi.first.wpilibj.Counter;
+import edu.wpi.first.wpilibj.AnalogTriggerOutput.AnalogTriggerType;
+import edu.wpi.first.wpilibj.Encoder;
+
+
+public class SteeringEncoder extends Runnable {
+	Logger centerLogger;
+	Logger counter;
+	public Counter steerCounter;
+	public AnalogInput encoder;
+	public AnalogTrigger turnTrigger;
+	public double offset;
+	private double voltage;
+	public int countOffset;
+	double minVoltage = 0;
+	double maxVoltage = 5;
+	double oldVoltage;
+	double angle;
+	double lastStopWatch = 0;
+	double degreesPerRotation = 102.85714285714285714285714285714;
+	int wheel;
+	
+	public SteeringEncoder(int channel, int _wheel) throws FileNotFoundException, UnsupportedEncodingException,IOException{
+		encoder = new AnalogInput(channel);
+		
+		turnTrigger = new AnalogTrigger(encoder);
+		turnTrigger.setLimitsVoltage(1.0, 4.0);
+		turnTrigger.setFiltered(true);
+		
+		steerCounter = new Counter();
+		steerCounter.setUpDownCounterMode();
+		steerCounter.setUpSource(turnTrigger, AnalogTriggerType.kRisingPulse);
+		steerCounter.setDownSource(turnTrigger, AnalogTriggerType.kFallingPulse);
+		
+		wheel = _wheel;
+		
+		offset = 0;
+		
+		counter = new Logger(new String[] {""},"/usr/local/frc/logs/zcounter"+ wheel +".txt");
+		centerLogger = new Logger(new String[] {""},"/usr/local/frc/logs/zcenter"+ wheel +".txt");
+		
+		String str = "0";
+		str = centerLogger.read(1)[0];
+		centerLogger.closeReader();
+		offset = Util.map( encoder.getVoltage(), minVoltage, maxVoltage, 0, degreesPerRotation);
+		
+//		try{
+//			offset = Double.parseDouble(str);
+//		}catch(NumberFormatException e){
+//			offset = 0;
+//		}
+		steerCounter.reset();
+		
+		String s = "0";
+		counter.makeReader();
+//		try{
+//			 s = counter.read(1)[0];
+//			 counter.closeReader();
+//		}catch(IOException e){e.printStackTrace();}
+		if (s == null)countOffset = 0;
+		else countOffset = Integer.parseInt(s);
+		countOffset=0;
+		steerCounter.reset();
+	}
+	
+	@Override
+	public void start(int periodMS){
+		voltage = encoder.getVoltage();
+		oldVoltage = voltage;
+		super.start(periodMS);
+	}
+	
+	@Override
+	public void update(){
+		super.update();
+		
+		counter.makeWriter();
+		int temp = countOffset+steerCounter.get();
+		counter.write(temp);
+		counter.closeWriter();
+		
+		voltage = encoder.getVoltage();
+	}
+	
+	@Override
+	public void stop(){
+		super.stop();
+	}
+	
+	public void trimCenter(double trim){
+		offset+=trim;
+	}
+	
+
+	public void writeOffset(){
+		System.out.print(wheel + "  writing");
+		centerLogger.makeWriter();
+//		offset=offset%degreesPerRotation; //THIS IS OUR OFFFSET WHICH WE ARE CHANGING
+		offset = Util.map( encoder.getVoltage(), minVoltage, maxVoltage, 0, degreesPerRotation);
+		
+		if (offset < 0)offset+=degreesPerRotation;
+		centerLogger.write(offset);
+		centerLogger.closeWriter();
+		System.out.println(offset);
+		countOffset=0;
+		counter.makeWriter();
+		counter.write(countOffset);
+		counter.closeWriter();
+		steerCounter.reset();
+	}
+	
+	public double getAngleDegrees(){
+		angle = (((countOffset+ steerCounter.get())*degreesPerRotation + Util.map( encoder.getVoltage(), minVoltage, maxVoltage, 0, degreesPerRotation))- offset)%360;
+		if(angle<0) angle+=360;
+		return angle;
+	}
+	public double getRawVoltage(){
+		return voltage;
+	}
+}
